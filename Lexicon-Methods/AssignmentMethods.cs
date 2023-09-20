143970a61a0d4d9f9475b40b3b4635f968be3214
Thord Möller <thord_moller_92@hotmail.com>
2023-09-20 04:34:18 +0200

    Implemented task 3 and updated 1 and 2 to use 3 to make it a lot shorter

--- Lexicon-Methods/AssignmentMethods.cs
+++ Lexicon-Methods/AssignmentMethods.cs
@@ -3,22 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Lexicon_Methods
 {
     internal class AssignmentMethods
     {
         /// <summary>
-        /// Asks user for their name and a statement. Prints both
+        /// Asks user to enter its name
         /// </summary>
-        public static void DisplayUserName() {
-            string? name;
-            Console.WriteLine("Please enter your name");
-            name = Console.ReadLine();
-            Console.WriteLine(name);
+        /// <returns>Entered name as string</returns>
+        public static string DisplayUserName() {
+            return AskUser("Please enter your name");
         }
 
+        /// <summary>
+        /// Asks user to enter a word.
+        /// </summary>
+        /// <returns>entered word prefixed with a hashtag</returns>
+        public static string PrefixWithHashtag() {
+            return "#" + AskUser("Please enter a word");
+        }
 
+        /// <summary>
+        /// Asks the user something
+        /// </summary>
+        /// <param name="question">question for the user</param>
+        /// <returns>The users answer</returns>
+        public static string AskUser(string question) {
+            string? answer;
+            WriteLine(question);
+            answer = ReadLine();
+            return answer ?? "No answer was given";
+        }
 
     }
 }
--- Lexicon-Methods/Program.cs
+++ Lexicon-Methods/Program.cs
@@ -1,11 +1,14 @@
 ﻿using static Lexicon_Methods.AssignmentMethods;     //with this line i don't have to specify the static AssignmentMethods class everytime i call a method from it
+using static System.Console;    //same thing for the Console
 
 namespace Lexicon_Methods
 {
     internal class Program
     {
         static void Main(string[] args) {
-            DisplayUserName();
+            WriteLine(DisplayUserName());
+            WriteLine(PrefixWithHashtag());
+            WriteLine(AskUser("ett nummer"));
         }
     }
 }
